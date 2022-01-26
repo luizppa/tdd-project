01c671c4d43b32e1f08187850a867d96396e7cb4
Luiz Philippe <luizphilippep@gmail.com>
2022-01-26 19:56:18 -0300

    Apresentar Múltiplos Itens na Listagem

--- lists/views.py
+++ lists/views.py
@@ -11,6 +11,7 @@ def home_page(request):
     Item.objects.create(text=new_item_text)
     return redirect('/')
 
+  items = Item.objects.all()
   return render(request, 'home.html', {
-      'new_item_text': new_item_text
+      'items': items
   })
