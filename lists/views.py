2dfb03d0c5890986f74852462f3fa0914476ffca
Luiz Philippe <luizphilippep@gmail.com>
2022-01-28 20:05:48 -0300

    Validando os erros na view

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
 
@@ -8,9 +8,15 @@ def home_page(request):
   return render(request, 'home.html')
 
 def new_list(request):
-  my_list = List.objects.create()
-  Item.objects.create(text=request.POST['item_text'], list=my_list)
-  return redirect(f'/lists/{my_list.id}/')
+  list_ = List.objects.create()
+  item = Item.objects.create(text=request.POST['item_text'], list=list_)
+  try:
+    item.full_clean()
+  except ValidationError:
+    error = "You can't have an empty list item"
+    return render(request, 'home.html', {"error": error})
+    
+  return redirect(f'/lists/{list_.id}/')
 
 def view_list(request, list_id):
   my_list = List.objects.get(id=list_id)
