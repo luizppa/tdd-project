9ded60b138c45bc24f670096bfedad4364ed1212
Luiz Philippe <luizphilippep@gmail.com>
2022-01-28 22:26:53 -0300

    De volta ao teste funcional - PARTE 2

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -51,6 +51,17 @@ class ListViewTest(TestCase):
     self.assertEqual(new_item.text, 'A new item for an existing list')
     self.assertEqual(new_item.list, correct_list)
 
+  def test_validation_errors_end_up_on_lists_page(self):
+    list_ = List.objects.create()
+    response = self.client.post(
+        f'/lists/{list_.id}/',
+        data={'item_text': ''}
+    )
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'list.html')
+    expected_error = escape("You can't have an empty list item")
+    self.assertContains(response, expected_error)
+
 
   def test_redirects_to_list_view(self):
     other_list = List.objects.create()
